8d501827b1ae9cce742a833565e4dcf781f9caca
Young well <yangww@onein.com>
2023-05-15 15:07:22 +0800

    feat: init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <button @click="jump">日志</button>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+import { ref } from "vue";
+import Taro, { onPullDownRefresh } from "@tarojs/taro";
+import "./index.scss";
+
+export default {
+  setup() {
+    const msg = ref("Hello world");
+    const jump = () => {
+      Taro.navigateTo({
+        url: "pages/log/index",
+      });
+    };
+    Taro.usePullDownRefresh(() => {
+      setTimeout(() => {
+        Taro.stopPullDownRefresh();
+      }, 2000);
+    });
+    return {
+      msg,
+      jump,
+    };
+  },
+};
+</script>
+<style>
+.index {
+  padding: 20px;
+}
+</style>
---
+++ src/pages/log/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <view class="index">
+    <text>Top{{ msg }}</text>
+  </view>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+import { ref } from "vue";
+import Taro from "@tarojs/taro";
+import "./index.scss";
+
+export default {
+  setup() {
+    const msg = ref("log page");
+    Taro.usePullDownRefresh(() => {
+      setTimeout(() => {
+        Taro.stopPullDownRefresh();
+      }, 2000);
+    });
+    return {
+      msg,
+    };
+  },
+};
+</script>
+<style>
+.index {
+  padding: 20px;
+}
+</style>
